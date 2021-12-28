27e218ae6dde9c1b998610ff38532696244d47b5
pkuWu <93828446+pkuWu@users.noreply.github.com>
2021-12-28 22:16:36 +0800

    修改数据位置

--- Dynamic_Hedge_System_for_Options/02_src/classes/basicData/basicData.py
+++ Dynamic_Hedge_System_for_Options/02_src/classes/basicData/basicData.py
@@ -1,6 +1,6 @@
 import pickle
 class BasicData:
-    with open('./data/cleandata.pkl', 'rb') as file:
+    with open('./data/python_data/cleandata.pkl', 'rb') as file:
         basicData = pickle.load(file)
     # 这里要把基础数据读入
     def __new__(cls):
