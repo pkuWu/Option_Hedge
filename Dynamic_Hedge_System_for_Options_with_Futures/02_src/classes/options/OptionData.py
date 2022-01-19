6ffec25462699bee359339e5b74edff93e5b10e3
Zhang-Yumeng <zhangym_0707@163.com>
2022-01-19 22:06:05 +0800

    Option_Contract类
    BullSpread待优化，其余种类的待添加

--- Dynamic_Hedge_System_for_Options_with_Futures/02_src/classes/options/OptionData.py
+++
@@ -1,9 +0,0 @@
-from .Vanilla import VanillaCall,VanillaPut
-from ..basicData.basicData import BasicData
-import pandas as pd
-class OptionData:
-    def __init__(self):
-        pass
-
-    def set_option_by_dict(self,option_class,option_position,option_paras):
-        pass
