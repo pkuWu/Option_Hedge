ced16e386f28ee5670f29f6da7dc36e062180313
Zhang-Yumeng <zhangym_0707@163.com>
2022-01-19 16:44:19 +0800

    Update basicData.py

--- Dynamic_Hedge_System_for_Options_with_Futures/02_src/classes/basicData/basicData.py
+++ Dynamic_Hedge_System_for_Options_with_Futures/02_src/classes/basicData/basicData.py
@@ -1,5 +1,13 @@
 import pickle
+
 class BasicData:
-    # 这里要把基础数据读入
+    with open('./data/python_data/clean_data.pickle', 'rb') as file:
+        basic_data = pickle.load(file)
+
+    ALL_TRADE_DATES = basic_data['trade_date']
+    PRICE_DICT = dict()
+    PRICE_DICT['open'] = basic_data['open']
+    PRICE_DICT['close'] = basic_data['close']
+
     def __new__(cls):
         return cls
