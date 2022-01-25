a6e0700004f0c718abcdd54fd9eb72ecbcebe2a0
qwq-cwq <759843432@qq.com>
2022-01-26 00:38:32 +0800

    添加了WW对冲

--- Dynamic_Hedge_System_for_Options_with_Futures/02_src/test/test_for_Delta_Strategy.py
+++ Dynamic_Hedge_System_for_Options_with_Futures/02_src/test/test_for_Delta_Strategy.py
@@ -2,15 +2,19 @@ from classes.strategy.Delta_Strategy import *
 from classes.options.Option_Contract import Option_Contract
 from datetime import date
 
+option = Option_Contract().create_option_portfolio(option_class='VanillaCall', option_position=1,
+                                                   option_paras={'stock_index_code': '000300.SH', 'start_date': date(2019, 1, 2),
+                                                    'end_date': date(2019, 3, 29), 'K': 3000, 'r': 0.04,
+                                                    'option_fee': 1780800})
+
+##WW
+wwhedge = WW_Hedge().get_option_info(option)
+ww_hedge_df = wwhedge.calculate_target_delta_interval()
 
 #HedgeAll
-option = Option_Contract().create_option_portfolio(option_class='VanillaCall',option_position=1, stock_index_code='000300.SH', start_date=date(2019,1,2),end_date=date(2019,3,29), K=3000, r=0.04,option_fee=1780800) # 实例化且传入参数
 hedgeall = HedgeAll().get_option_info(option) # 将策略类实例化并传入参数
-hedgeall.calculate_target_delta() # 计算需要对冲的cash_delta
-hedgeall_target_delta = hedgeall.get_target_delta() # 返回需要对冲的cash_delta
+hedgeall_target_delta = hedgeall.calculate_target_delta() # 计算需要对冲的cash_delta
 
 #HedgeHalf
-option = Option_Contract().create_option_portfolio(option_class='VanillaCall',option_position=1, stock_index_code='000300.SH', start_date=date(2019,1,2),end_date=date(2019,3,29), K=3000, r=0.04,option_fee=1780800) # 实例化且传入参数
 hedgehalf = HedgeHalf().get_option_info(option) # 将策略类实例化并传入参数
-hedgehalf.calculate_target_delta() # 计算需要对冲的cash_delta
-hedgehalf_target_delta = hedgehalf.get_target_delta()# 返回需要对冲的cash_delta
+hedgehalf_target_delta = hedgehalf.calculate_target_delta() # 计算需要对冲的cash_delta
